a61afc6481906f007b62ec81861e0c69715359f5
Sebastian Stryczek <sebastian@stryczek.eu>
2016-05-28 13:06:21 +0200

    First working version of searching

--- app/components/search-panel.js
+++ app/components/search-panel.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     actions: {
         searchAction(searchText) {
-            console.log(searchText);
+            this.sendAction('searchAction', searchText);
         }
     }
 });
--- app/controllers/search.js
+++ app/controllers/search.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    actions: {
+        doSearch(searchText) {
+            this.transitionToRoute('search', {
+              queryParams: {
+                  text: searchText,
+                  licence: 7,
+                  page: 1
+              }
+          });
+        }
+    }
 });
