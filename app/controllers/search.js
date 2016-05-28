14197ba06da5cd86475abb525cdc934c5a004dcc
Sebastian Stryczek <sebastian@stryczek.eu>
2016-05-28 14:51:19 +0200

    Added Prev/Next buttons

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -1,15 +1,37 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    actions: {
-        doSearch(searchText) {
-            this.transitionToRoute('search', {
-              queryParams: {
-                  text: searchText,
-                  licence: 7,
-                  page: 1
-              }
-          });
-        }
+  currentSearch: {
+    text: "a",
+    licence: 7,
+    page: 1
+  },
+  doSearch() {
+    this.transitionToRoute('search', {
+      queryParams: {
+        text: this.currentSearch.text,
+        licence: this.currentSearch.licence,
+        page: this.currentSearch.page
+      }
+    });
+  },
+  actions: {
+    searchAction(searchText) {
+      this.currentSearch.text = searchText;
+      this.currentSearch.licence = 7;
+      this.currentSearch.page = 1;
+      this.doSearch();
+    },
+    /* Temporary actions
+     * TODO Change it to infinity scroll
+     */
+    searchNextPage() {
+      this.currentSearch.page += 1;
+      this.doSearch();
+    },
+    searchPrevPage() {
+      this.currentSearch.page -= 1;
+      this.doSearch();
     }
+  }
 });
